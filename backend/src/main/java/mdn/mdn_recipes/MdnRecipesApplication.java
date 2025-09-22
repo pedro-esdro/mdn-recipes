45f5980f895e5320bae14643b343359a7f444a88
pedro-esdro <pehen092@gmail.com>
2025-09-22 20:35:40 -0300

    Create startup class

---
+++ backend/src/main/java/mdn/mdn_recipes/MdnRecipesApplication.java
@@ -0,0 +1,12 @@
+package mdn.mdn_recipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdnRecipesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MdnRecipesApplication.class, args);
+    }
+}
