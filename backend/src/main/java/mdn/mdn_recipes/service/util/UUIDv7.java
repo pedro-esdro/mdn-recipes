735e91d8a5cc6aeb650b2938f41c32d48539df33
pedro-esdro <pehen092@gmail.com>
2025-09-24 22:56:50 -0300

    Create Recipe, Step and Ingredient entities and UUIDv7 utility class with create method

---
+++ backend/src/main/java/mdn/mdn_recipes/service/util/UUIDv7.java
@@ -0,0 +1,15 @@
+package mdn.mdn_recipes.service.util;
+
+import com.github.f4b6a3.uuid.UuidCreator;
+
+import java.util.UUID;
+
+public final class UUIDv7 {
+
+    private UUIDv7() {}
+
+    public static UUID create() {
+        return UuidCreator.getTimeOrderedEpoch();
+    }
+
+}
