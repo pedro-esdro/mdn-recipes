102d5ef3998025a15019853a0b9183fe15a43de4
pedro-esdro <pehen092@gmail.com>
2025-09-25 00:05:36 -0300

    Fix migration, add flyway dependencies and add datasource, jpa and flyway configs in .properties

--- backend/src/main/resources/db/migration/V1__create_recipe_ingredient_step_tables.sql
+++ backend/src/main/resources/db/migration/V1__create_recipe_ingredient_step_tables.sql
@@ -2,16 +2,16 @@ CREATE TABLE recipe (
     id UUID PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     description TEXT,
-    servings int NOT NULL,
+    servings INT NOT NULL,
     preparation_time TIME NOT NULL,
-    image_path TEXT NOT NULL
+    image_path TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE ingredient (
     id UUID PRIMARY KEY,
     recipe_id UUID NOT NULL REFERENCES recipe(id) ON DELETE CASCADE,
-    name VARCHAR(100) NOT NULL
+    name VARCHAR(100) NOT NULL,
     quantity VARCHAR(30) NOT NULL
 );
 
@@ -22,5 +22,5 @@ CREATE TABLE step (
     step_order INT NOT NULL
 );
 
-CREATE INDEX idx_ingredients_recipe ON ingredients(recipe_id);
-CREATE INDEX idx_steps_recipe_order ON steps(recipe_id, step_order);
+CREATE INDEX idx_ingredient_recipe ON ingredient(recipe_id);
+CREATE INDEX idx_step_recipe_order ON step(recipe_id, step_order);
